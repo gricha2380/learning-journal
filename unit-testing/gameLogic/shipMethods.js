4eeefc36375ba03e7be6e6d47f9c7b3a5351287c
Gregor Richardson <gregor.richardson@gmail.com>
2018-02-25 10:08:08 -0800

    starts ship damage function

--- unit-testing/gameLogic/shipMethods.js
+++ unit-testing/gameLogic/shipMethods.js
@@ -6,10 +6,21 @@ let checkforShip = (player,coordinateGuess) => {
             return (shipCoordinates[0]===coordinateGuess[0] && shipCoordinates[1]===coordinateGuess[1])
         })[0]
 
-        if(!shipPresent) {
-            return false;
+        if(shipPresent) {
+            return true;
         }
     }
+    return false;
 }
 
+let shipDamage = (ship, coordinateGuess) => {
+    ship.damage.push(coordinateGuess);
+    // console.log('user guessed',coordinateGuess,typeof coordinateGuess)
+    // for (let i=0;i<ship.locations.length;i++) {
+    //     console.log('checking location',ship.locations[i], typeof ship.locations[i])
+    //     if(ship.locations[i]==[coordinateGuess]) {console.log('it matches!');ship.damage.push(coordinateGuess);}
+    // }
+    // console.log('ship damage report',ship.damage)
+}
 module.exports.checkforShip = checkforShip;
+module.exports.shipDamage = shipDamage;
--- unit-testing/test/shipTests.js
+++ unit-testing/test/shipTests.js
@@ -4,7 +4,7 @@ let expect = require('chai').expect;
 describe('check for ship',()=>{
     let checkforShip = require('../gameLogic/shipMethods').checkforShip;
     it('should show no ship at a given coordinate for specified player',()=>{
-        let player = {
+        player = {
             "ships" : [
                 {
                     "locations": [[0,0]]
@@ -12,5 +12,56 @@ describe('check for ship',()=>{
             ]
         }
         expect(checkforShip(player,[3,3])).to.be.false;
+    })
+
+    it('ships located at more than one coordinate',()=>{
+        player = {
+            "ships" : [
+                {
+                    "locations": [[0,0],[0,1]]
+                }
+            ]
+        }
+        expect(checkforShip(player,[0,1])).to.be.true;
+        expect(checkforShip(player,[0,0])).to.be.true;
+        expect(checkforShip(player,[3,3])).to.be.false;
+    })
+
+    it('checking multiple ships',()=>{
+        player = {
+            "ships" : [
+                {
+                    "locations": [[0,0],[0,1]]
+                },
+                {
+                    "locations": [[1,0],[1,1]]
+                },
+                {
+                    "locations": [[2,0],[2,1],[2,2],[2,3]]
+                }
+            ]
+        }
+        expect(checkforShip(player,[0,1])).to.be.true;
+        expect(checkforShip(player,[0,0])).to.be.true;
+        expect(checkforShip(player,[1,0])).to.be.true;
+        expect(checkforShip(player,[1,1])).to.be.true;
+        expect(checkforShip(player,[2,4])).to.be.false;
+        expect(checkforShip(player,[2,3])).to.be.true;
+        expect(checkforShip(player,[4,2])).to.be.false;
+    })
+})
+
+
+// battle damage
+describe('ship damage',()=>{
+    let shipDamage = require('../gameLogic/shipMethods').shipDamage;
+    it('register damage to a given ship in a given location',()=>{
+        let ship = {
+            locations: [[0,0]],
+            damage: []
+        }
+        shipDamage(ship,[0,0])
+        expect(ship.damage).to.not.be.empty;
+        expect(ship.damage[0]).to.deep.equal([0,0])
     })
 })
